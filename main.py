a3a680bc742fca5c51fb118a200490c4c15ede03
whoisfransys <franpassa10@gmail.com>
2023-01-25 21:48:59 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pickle
+
+class Vehiculo:
+    ruedas = 0
+    puertas = 0
+    motor = ""
+
+    def __init__(self, ruedas, puertas, motor):
+        self.ruedas = ruedas
+        self.puertas = puertas
+        self.motor = motor
+
+    def __str__(self):
+        return f"Es un vehiculo de {self.ruedas} ruedas, con {self.puertas} puertas y de color {self.motor}"
+
+
+auto1 = Vehiculo(4, 5, "Diesel")
+
+f = open("vehiculo.bin", "wb")
+pickle.dump(auto1, f)
+f.close()
+
+f = open("vehiculo.bin", "rb")
+auto = pickle.load(f)
+print(auto)
+
+
